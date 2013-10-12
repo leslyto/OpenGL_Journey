af11a8b78aa4d7869e62e578771defa8147021ac
gecaka <gecaka12@gmail.com>
2013-10-12 18:08:34 +0100

    Changes to deck
    Started construcktion of deck class

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,35 @@
 
-public class Deck {
+public class Deck{
+	
+/// Private //////////////////////////////////////////////////////////////////////////////////	
+	
+	private Card[] cards = new Card[52]; //Cards in deck
 
+	private Card[] delt = new Card[52]; //Cards already delt 
+	
+/// Public ///////////////////////////////////////////////////////////////////////////////////
+	
+	public Card getCard(int f_position)
+	{
+		return  cards[position];
+	}
+	
+	public Card findDeltCard(String f_type,String f_suit )
+	{
+		for(short i = 0;i<delt.length;i++)
+		{
+			if(delt[i])
+		}
+		
+		return 
+	}
+	
+	
+/// Constructor //////////////////////////////////////////////////////////////////////////////	
+	
+	public Deck() 
+	{
+		// sus for loop
+	}
+	
 }
