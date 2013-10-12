9c159639479ab797c83a44a3b2d147d84e9a245f
leslyto <stefanstoev1993@gmail.com>
2013-10-12 18:08:22 +0100

    Card class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,33 @@
 
 public class Card {
-
+	//private
+	private String type;
+	private String suit;
+	private String value;
+	
+	//public
+	public void setType(String typeln)
+	{
+		type = typeln;
+	}
+	public void setSuit(String suitln)
+	{
+		suit = suitln;
+	}
+	public void setValue(String valueln)
+	{
+		value = valueln;
+	}
+	public String getType()
+	{
+		return type;
+	}
+	public String getSuit()
+	{
+		return suit;
+	}
+	public String getValue()
+	{
+		return value;
+	}
 }
