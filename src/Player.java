3735fc42ee023e4c738b11849326bf64bf75ca39
leslyto <stefanstoev1993@gmail.com>
2013-10-12 19:19:45 +0100

    Player i oshte

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,31 @@
 
 public class Player {
+	//private
+	private Card[] current_deck = new Card[52];
+	private void playWar()
+	{
+		/*
+		deal 1 card
+		if(player_card > comp_card)
+		{
+			table cards -> player cards
+		}
+		else if(player_card < comp_card)
+		{
+			table_cards -> comp_cards
+		}
+		else
+		{
+			another war
+		}
+		*/
+	}
+	//public
+	public void playCard()
+	{
+		
+	}
 
+	
+	
 }
